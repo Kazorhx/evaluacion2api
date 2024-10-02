8ab8eda6780c9699f7dfa9b4946f00ddc377a0fc
zetam <zetam@RYUUZAIKEN>
2024-10-02 00:31:10 -0300

    creacion de TareaResponse.cs y cambio de nombre en archivos

---
+++ evaluacion2api/Responses/TareaResponse.cs
@@ -0,0 +1,27 @@
+﻿using evaluacion2api.Models;
+
+namespace evaluacion2api.Responses
+{
+        public class TareaResponse : ResponseBase<Tarea>
+        {
+        }
+
+        public class TareasResponse : ResponseBase<List<Tarea>>
+        {
+        }
+
+        public class NuevaTareaResponse : ResponseBase<bool>
+        {
+        }
+
+        public class UpdateTareaResponse : ResponseBase<bool>
+        {
+        }
+
+        public class DeleteTareaResponse : ResponseBase<bool>
+        {
+        }
+
+    }
+
+
